21b9495fadb12f6b3105550c33db1ffd1be2639a
Aasipenka <aasipenka@elinext.com>
2022-03-27 19:38:55 +0300

    Resolve conflict

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,7 +5,6 @@ import { Users } from './model/users.model';
 
 @Injectable()
 export class UsersService {
-<<<<<<< HEAD
   //1-администратор 2-ОМА 3-ОМТСиК
   private availableUsers: Array<string> = ['Admin'];
 
@@ -36,26 +35,4 @@ export class UsersService {
       include: { all: true },
     });
   }
-=======
-    //1-администратор 2-ОМА 3-ОМТСиК
-    private availableUsers: Array<string> = ['Admin'];
-    
-    constructor(@InjectModel(Users) private userRepository: typeof Users) {}
-
-    public async createUsers(newUser: UsersDto) {
-        if(this.availableUsers.includes(newUser.creatorInfo.role) && this.getUserByEmail(newUser.creatorInfo.email) || (await this.getAllUsers()).length === 0) {
-            return await this.userRepository.create(newUser);
-        } else {
-            throw new HttpException('There is no such user or there is no access.', HttpStatus.BAD_REQUEST);
-        }
-    }
-  
-    public async getAllUsers() {
-        return await this.userRepository.findAll({include: {all: true}});
-      }
-    
-    public async getUserByEmail(email: string) {
-      return await this.userRepository.findOne({where: {email}, include: {all: true}})
-    }
->>>>>>> 1efde9d5eb10719d158a4cd80e2df4ac813b5786
 }
